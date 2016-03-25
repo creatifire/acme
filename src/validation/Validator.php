4d96059beeef844224e7d080d0dc74e63c219950
Arif Sufi <arifsufi@ape4.com>
2016-03-25 16:45:15 -0400

    Cleaned up formatting

--- src/validation/Validator.php
+++ src/validation/Validator.php
@@ -16,25 +16,25 @@ class Validator
                     $exploded = explode(":", $rule);
 
                     switch ($exploded[0]) {
-            case 'min':
-              $min = $exploded[1];
-              if (!Valid::stringType()->length($min)->Validate($_REQUEST[$name])) {
-                  $errors[] = $name . " must be at least " . $min . " characters long!";
-              }
-              break;
+                        case 'min':
+                          $min = $exploded[1];
+                          if (!Valid::stringType()->length($min)->Validate($_REQUEST[$name])) {
+                              $errors[] = $name . " must be at least " . $min . " characters long!";
+                          }
+                          break;
 
-            case 'email':
-              if (!Valid::email()->validate($_REQUEST[$name])) {
-                  $errors[] = $name . " must be a valid email address.";
-              }
-              break;
+                        case 'email':
+                          if (!Valid::email()->validate($_REQUEST[$name])) {
+                              $errors[] = $name . " must be a valid email address.";
+                          }
+                          break;
 
-            case 'equalTo':
-              if (!Valid::equals($_REQUEST[$name])->validate($_REQUEST[$exploded[1]])) {
-                  $errors[] = $name . " must equal " . $exploded[1];
-              }
-            default:
-          }
+                        case 'equalTo':
+                          if (!Valid::equals($_REQUEST[$name])->validate($_REQUEST[$exploded[1]])) {
+                              $errors[] = $name . " must equal " . $exploded[1];
+                          }
+                        default:
+                      }
                 }
             } else {
                 $errors[] = "No value found for " . $name;
