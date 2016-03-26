7241fc73599f2ac8e682f97fcb1772b9f08d0d8a
Arif Sufi <arifsufi@ape4.com>
2016-03-26 16:00:18 -0400

    Configure index to allow entry of content in the routes table.

--- routes.php
+++ routes.php
@@ -4,4 +4,9 @@ $router->map('GET', '/', 'Acme\Controllers\PageController@getShowHomePage', 'hom
 $router->map('GET', '/register', 'Acme\Controllers\RegisterController@getShowRegisterPage', 'register');
 $router->map('POST', '/register', 'Acme\Controllers\RegisterController@postShowRegisterPage', 'register_post');
 $router->map('GET', '/login', 'Acme\Controllers\RegisterController@getShowLoginPage', 'login');
-$router->map('GET', '/about', 'Acme\Controllers\PageController@getShowPage', 'generic_page' );
+$router->map('GET', '/[*]', 'Acme\Controllers\PageController@getShowPage', 'generic_page' );
+// $router->map('GET', '/test', function() {
+//     $testimonials = Acme\models\User::find(2)->testimonials()->get();
+//     // $testimonials = $user->testimonials()->get();
+//     dd($testimonials);
+// });
--- src/models/Testimonial.php
+++ src/models/Testimonial.php
@@ -4,5 +4,8 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Testimonial extends Eloquent
 {
-
+    public function user()
+    {
+        $this->hasOne('Acme\models\User');
+    }    
 }
